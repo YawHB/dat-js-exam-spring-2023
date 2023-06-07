d87e5e54b1e3acea65e8f93a9237d9c62ba54205
YawHB <yawhboateng@gmail.com>
2023-06-07 20:01:39 +0200

    Completed Øvelse 1.2 (Environment 7)

--- environment-07/script.js
+++ environment-07/script.js
@@ -1,1 +1,54 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+let posts = [];
+
+async function start() {
+    posts = await getPosts();
+
+    showPosts();
+}
+
+async function getPosts() {
+    const res = await fetch("posts.json");
+    return res.json();
+}
+
+function showPosts() {
+    posts.sort((b, a) => a.likes - b.likes);
+    document.querySelector("#posts-list").innerHTML = "";
+    for (const post of posts) {
+        const html = /*html*/ `
+        <article>
+             <img src="${post.image}" alt="${post.caption}" />
+             <h2>${post.caption}</h2>
+             <p>Likes: ${post.likes}</p>
+             <button class="like">Like</button>
+             <button class="unlike">Unlike</button>
+        </article>
+        
+        `;
+
+        document
+            .querySelector("#posts-list")
+            .insertAdjacentHTML("beforeend", html);
+
+        document
+            .querySelector("#posts-list article:last-child .like")
+            .addEventListener("click", () => addOneLike(post));
+
+        document
+            .querySelector("#posts-list article:last-child .unlike")
+            .addEventListener("click", () => minusOneLike(post));
+    }
+}
+
+function addOneLike(post) {
+    post.likes++;
+    showPosts();
+}
+function minusOneLike(post) {
+    post.likes--;
+    showPosts();
+}
