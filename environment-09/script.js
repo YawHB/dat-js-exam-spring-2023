af2c123e5c0c73f0b3dc4f21452d8cd36fe7c0de
YawHB <yawhboateng@gmail.com>
2023-06-08 20:04:57 +0200

    Deleted code

--- environment-09/script.js
+++ environment-09/script.js
@@ -1,52 +1,13 @@
 "use strict";
-
 window.addEventListener("load", start);
 
 let posts = [];
-let publishedPosts = [];
 async function start() {
-    posts = await fetchPosts();
+    posts = await getPosts();
     console.log(posts);
-    publishedPosts = posts.filter((post) => post.published);
-    showPosts(publishedPosts);
-    document
-        .querySelector("#filter-form")
-        .addEventListener("change", showAllPosts);
 }
 
-async function fetchPosts() {
-    const res = await fetch("posts.json");
+async function getPosts() {
+    const res = await fetch("./posts.json");
     return res.json();
-}
-
-function showPosts(posts) {
-    document.querySelector("#posts-list").innerHTML = "";
-    for (const post of posts) {
-        const html = /*html*/ `
-        
-        <article>
-                    <img src = "${post.image}" alt="${post.caption}" />
-                    <h2>${post.caption}</h2>
-                    <p>Likes: ${post.likes}</p>
-                </article>
-        
-        `;
-        document
-            .querySelector("#posts-list")
-            .insertAdjacentHTML("beforeend", html);
-    }
-}
-
-function showAllPosts(event) {
-    // const isChecked = document.querySelector("#show-all-checkbox").checked;
-
-    const isChecked = event.target.checked;
-    // const isChecked = input.checked;
-
-    console.log(isChecked);
-    if (isChecked) {
-        showPosts(posts);
-    } else if (!isChecked) {
-        showPosts(publishedPosts);
-    }
 }
