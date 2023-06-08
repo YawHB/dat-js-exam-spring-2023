5a55a5173561aa214a0a0d53e65b0ce25841c85a
YawHB <yawhboateng@gmail.com>
2023-06-08 20:16:51 +0200

    Completed Øvelse 10 1.3

--- environment-09/script.js
+++ environment-09/script.js
@@ -1,13 +1,1 @@
 "use strict";
-window.addEventListener("load", start);
-
-let posts = [];
-async function start() {
-    posts = await getPosts();
-    console.log(posts);
-}
-
-async function getPosts() {
-    const res = await fetch("./posts.json");
-    return res.json();
-}
--- environment-10/script.js
+++ environment-10/script.js
@@ -1,1 +1,49 @@
 "use strict";
+window.addEventListener("load", start);
+
+let posts = [];
+let publishedPosts = [];
+async function start() {
+    document
+        .querySelector("#filter-form")
+        .addEventListener("change", showPostsCheck);
+    posts = await getPosts();
+    filterPublishedPosts();
+    showPosts(publishedPosts);
+}
+
+async function getPosts() {
+    const res = await fetch("./posts.json");
+    return res.json();
+}
+
+function filterPublishedPosts() {
+    publishedPosts = posts.filter((post) => post.published);
+}
+
+function showPosts(posts) {
+    document.querySelector("#posts-list").innerHTML = "";
+    for (const post of posts) {
+        const html = /*html*/ `
+        <article>
+                    <img src="${post.image}" alt="${post.caption}" />
+                    <h2>${post.caption}</h2>
+                    <p>Likes: ${post.likes}</p>
+                </article>
+        
+        `;
+        document
+            .querySelector("#posts-list")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
+
+function showPostsCheck(event) {
+    const isAllPosts = event.target.checked;
+    console.log(isAllPosts);
+    if (isAllPosts) {
+        showPosts(posts);
+    } else {
+        showPosts(publishedPosts);
+    }
+}
